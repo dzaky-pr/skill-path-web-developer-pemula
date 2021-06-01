4a28714c3df23e46a38282c37e122086a89d83c8
Dzaky Rifai <76737872+dzaky-pr@users.noreply.github.com>
2021-06-01 10:06:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -56,33 +56,33 @@ function resetForm() {
   document.getElementById('myForm').reset();
 }
 
-//22
-function submit() {
-  if (validateFormData(getFormData()) == false) {
-    warning.innerHTML = 'Periksa form anda sekali lagi';
-  } else {
-    let obj = {
-    firstName: document.getElementById('first-name').value;
-    lastName: document.getElementById('last-name').value;
-    warning.innerHTML = '';
-    alert(`thank you ${obj.firstName} ${obj.lastName}`);
-  }
-}
+//22 ini buat tes tp diganti biar simple
+// function submit() {
+//   if (validateFormData(getFormData()) == false) {
+//     warning.innerHTML = 'Periksa form anda sekali lagi';
+//   } else {
+//     let obj = {
+//     firstName: document.getElementById('first-name').value;
+//     lastName: document.getElementById('last-name').value;
+//     warning.innerHTML = '';
+//     alert(`thank you ${obj.firstName} ${obj.lastName}`);
+//   }
+// }
+
+// simple greeting message
+function greeting() {
+  let obj = {
+    firstName: document.getElementById('first-name').value,
+    lastName: document.getElementById('last-name').value,
+ };
+  if (firstname, last-name === true) {
+ alert(`thank you ${obj.firstName} ${obj.lastName}`);
+  } //else {
+  //   warning.innerHTML = 'Check your form again';
+  // }
+//}
 
 // alert function
 //function underMaintenance() {
   //alert('Sorry, this website is under maintenance :)');
-}
-
-// greeting message
-//function greeting() {
-  //let obj = {
-    //firstName: document.getElementById('first-name').value,
-    //lastName: document.getElementById('last-name').value,
-  };
-  // if () {
- // alert(`thank you ${obj.firstName} ${obj.lastName}`);
-  // } else {
-  //   warning.innerHTML = 'Check your form again';
-  // }
-}
+//}
